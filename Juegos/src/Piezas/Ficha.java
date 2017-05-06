d97e088355c723c43280c0042620dc8240406016
Arsereg <contacto@ginomarin.com>
2017-05-05 18:28:29 -0600

    First commit

--- Juegos/src/Piezas/Ficha.java
+++ Juegos/src/Piezas/Ficha.java
@@ -3,19 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package juegos;
+package Piezas;
 
 /**
  *
  * @author arser
  */
-public class Juegos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
-    
+public abstract class Ficha {
+    public int x;
+    public int y;
 }
